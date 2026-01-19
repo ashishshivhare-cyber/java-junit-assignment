553938df1b63ccd306bf2d1bc5d7aa99aacffe34
ashishshivhare-cyber <ashish.shivhare@rungta.org>
2026-01-20 02:00:17 +0530

    Create Main.java
    adding the main logic

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static boolean isPrime(int n) {
+        // TODO: write your logic here
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // not used for grading
+    }
+}
